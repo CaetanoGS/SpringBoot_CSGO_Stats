4e1889955e3029935417ef3171b7b7f4edd1f621
Gustavo  Caetano <gustavo@Gustavo.mshome.net>
2020-06-11 18:48:12 +0200

    First commit

---
+++ eventsApp/src/main/java/com/eventsApp/eventsApp/EventsAppApplication.java
@@ -0,0 +1,13 @@
+package com.eventsApp.eventsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsAppApplication.class, args);
+	}
+
+}
---
+++ eventsApp/src/test/java/com/eventsApp/eventsApp/EventsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsApp.eventsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
