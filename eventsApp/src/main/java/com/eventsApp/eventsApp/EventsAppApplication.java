3dd95b6f1b38362daa1849426652423ac6fef663
Gustavo  Caetano <gustavo@MacBook-Pro.mshome.net>
2020-06-28 22:19:26 +0200

    Front end Done 1.0

--- eventsApp/src/main/java/com/eventsApp/eventsApp/EventsAppApplication.java
+++ eventsApp/src/main/java/com/eventsApp/eventsApp/EventsAppApplication.java
@@ -2,9 +2,13 @@ package com.eventsApp.eventsApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class EventsAppApplication {
+public class EventsAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventsAppApplication.class, args);
